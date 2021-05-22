b7ef8ead51d787b4591e386e4ab0531bdd582479
Ken Wang <edlevisken@gmail.com>
2021-05-23 00:41:42 +0800

    5/23 權限新增 / 員工管理版面修改

--- src/com/authority/model/AuthorityVO.java
+++ src/com/authority/model/AuthorityVO.java
@@ -4,7 +4,7 @@ public class AuthorityVO implements java.io.Serializable{
 	
 	private Integer empno;
 	private Integer function_no;
-	private String status;
+	private String auth_status;
 	
 	public Integer getEmpno() {
 		return empno;
@@ -18,10 +18,10 @@ public class AuthorityVO implements java.io.Serializable{
 	public void setFunction_no(Integer function_no) {
 		this.function_no = function_no;
 	}
-	public String getStatus() {
-		return status;
+	public String getAuth_status() {
+		return auth_status;
 	}
-	public void setStatus(String status) {
-		this.status = status;
+	public void setAuth_status(String auth_status) {
+		this.auth_status = auth_status;
 	}
 }
