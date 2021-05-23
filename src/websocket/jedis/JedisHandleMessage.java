c99f9e9a53f32daacfa656e32c89db858b398a46
Eason Lai <j093760@livemail.tw>
2021-05-23 22:57:26 +0800

    首頁通知會更新鈴鐺數量

--- src/websocket/jedis/JedisHandleMessage.java
+++ src/websocket/jedis/JedisHandleMessage.java
@@ -14,14 +14,27 @@ public class JedisHandleMessage {
 
 
 	private static JedisPool pool = JedisPoolUtil.getJedisPool();  // like datasource
-
-	public static void updateRead(String sender) {
+	public static List<String> updateRead(String sender) {
 		String key = sender;
+
 		Jedis jedis = null;
 		jedis = pool.getResource();
 		jedis.auth("123456");
-		jedis.lset(key, -1, "Y");
+		Long count = jedis.llen(key);
+		List<String> updateData = new ArrayList<String>();
+		for(long i = 0 ; i < count; i++) {
+			String fragment = jedis.lindex(key, i);
+			int index = fragment.lastIndexOf("read");
+			String slice1 = fragment.substring(0,index);
+			String slice2 ="read\":\"Y\"}";
+//			System.out.println(slice1+slice2);
+			String afterRead = slice1+slice2;
+			jedis.lset(key, i, afterRead);
+			updateData.add(afterRead);
+		}
+		System.out.println(updateData);
 		jedis.close();
+		return updateData;
 	}
 	
 	public static List<String> getHistoryMsg(String sender) {
