5e24b83d348aa995b0f8640b3c0db00b532e1405
TONY <a5372268@gmail.com>
2021-05-27 00:54:55 +0800

    更新theater,showtime controller

--- src/com/theater/controller/TheaterServlet.java
+++ src/com/theater/controller/TheaterServlet.java
@@ -198,7 +198,7 @@ public class TheaterServlet extends HttpServlet {
 				
 				/***************************3.修改完成,準備轉交(Send the Success view)*************/
 				req.setAttribute("theaterVO", theaterVO); // 資料庫update成功後,正確的的theaterVO物件,存入req
-				String url = "/back-end/theater/listOneTheater.jsp";
+				String url = "/back-end/theater/listAllTheater.jsp";
 				RequestDispatcher successView = req.getRequestDispatcher(url); // 修改成功後,轉交listOneEmp.jsp
 				successView.forward(req, res);
 
@@ -279,7 +279,7 @@ public class TheaterServlet extends HttpServlet {
 				theaterVO = theaterSvc.addTheater(theater_name, theater_type, seat_no, seat_name);
 				
 				/***************************3.新增完成,準備轉交(Send the Success view)***********/
-				String url = "/back-end/theater/listAllTheater.jsp";
+				String url = "/back-end/theater/listAllTheater.jsp?whichPage=999";
 				RequestDispatcher successView = req.getRequestDispatcher(url); // 新增成功後轉交listAllTheater.jsp
 				successView.forward(req, res);				
 				
